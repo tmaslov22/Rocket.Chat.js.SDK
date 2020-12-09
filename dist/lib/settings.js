070c2a71991dc1072c8bc1d1b4bb3d72c0a46774
Timur Maslov <tmaslov22@gmail.com>
2020-12-10 01:26:06 +0500

    Build with bug fixes

--- dist/lib/settings.js
+++ dist/lib/settings.js
@@ -1,5 +1,10 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+}
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 // Login settings - LDAP needs to be explicitly enabled
 exports.username = process.env.ROCKETCHAT_USER || 'bot';
 exports.password = process.env.ROCKETCHAT_PASSWORD || 'pass';
